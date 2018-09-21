85b9eafc3b9de0159ab32b92455a76f670e4b2f5
Bert <bertholindestinvil@gmail.com>
2018-09-20 23:38:04 -0400

    Testing number of cards in hand
    Created testing for size of hand and to see if the hand is coming back with the proper amount of cards

---
+++ src/main/java/poker/Hand.java
@@ -0,0 +1,12 @@
+package poker;
+
+import java.util.*;
+
+public class Hand {
+	ArrayList<String> hand = new ArrayList<String>();
+	
+	public int size() {
+		return hand.size();
+	}
+
+}
--- src/test/java/poker/HandTest.java
+++ src/test/java/poker/HandTest.java
@@ -1,5 +1,26 @@
 package poker;
 
-public class HandTest {
+import junit.framework.TestCase;
 
+public class HandTest extends TestCase{
+	
+	public void testDrawing() {
+		Hand p1 = new Hand();
+		Hand ai = new Hand();
+		
+		assertEquals(0, p1.size());
+		assertEquals(0, ai.size());
+		
+		p1.draw(5);
+		assertEquals(5, p1.size());
+		
+		for(int i = 0; i < 5; ++i) {
+			ai.draw(1);
+		}
+		assertEquals(5, ai.size());
+		
+	}
+	
+	
+	
 }
