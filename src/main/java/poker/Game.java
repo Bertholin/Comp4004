2c6d532c0a225a3b39355b27f1bc82ef738ad5be
Bert <bertholindestinvil@gmail.com>
2018-09-22 15:56:26 -0400

    Refactored/ added new test to the HandTest
    -Refactored/ added new test to the HandTest
    -moved my draw code into poker and will refactor later
    -Create two empty classes for Game

---
+++ src/main/java/poker/Game.java
@@ -0,0 +1,26 @@
+package poker;
+
+import java.io.*;
+import java.util.*;
+
+public class Game {
+
+	Scanner sc = null;
+
+	public void draw(int num) {
+
+		try {
+			sc = new Scanner(new File("cards.txt"));
+
+			while (sc.hasNextLine()) {
+				String card = sc.nextLine();
+				System.out.println(card);
+				//hand.add(card);
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		sc.close();
+	}
+}
--- src/main/java/poker/Hand.java
+++ src/main/java/poker/Hand.java
@@ -1,29 +1,14 @@
 package poker;
 
 import java.util.*;
-import java.io.*;
+
 
 public class Hand {
 	ArrayList<String> hand = new ArrayList<String>();
-	Scanner sc = null;
+
 	public int size() {
 		return hand.size();
 	}
 
-	public void draw(int num) {
-		
-		try {
-			sc = new Scanner(new File("cards.txt"));
-
-			while (sc.hasNextLine()) {
-				String card = sc.nextLine();
-				System.out.println(card);
-				hand.add(card);
-
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		sc.close();
-	}
+	
 }
