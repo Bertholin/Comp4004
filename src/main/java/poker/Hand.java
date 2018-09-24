b50c86e25819208323919b0bed791b5d9fa12b99
Bert <bertholindestinvil@gmail.com>
2018-09-23 22:40:51 -0400

    inital code for getting the suit and value

--- src/main/java/poker/Hand.java
+++ src/main/java/poker/Hand.java
@@ -2,7 +2,6 @@ package poker;
 
 import java.util.*;
 
-
 public class Hand {
 	ArrayList<String> hand = new ArrayList<String>();
 
@@ -13,27 +12,49 @@ public class Hand {
 	public void add(int i, String val) {
 		hand.add(i, val);
 	}
-	
+
 	public void add(String val) {
 		hand.add(val);
 	}
-	
+
 	public String get(int num) {
 		return hand.get(num);
 	}
-	
+
 	public void remove(int i) {
 		hand.remove(i);
 	}
-	
+
 	public void removeAll() {
 		hand.clear();
 	}
-	
+
 	public boolean validHand(Hand h) {
 		if (h.size() == 5)
 			return true;
 		return false;
 	}
-	
+
+	public String getSuit(String s) {
+
+		return s.substring(0, 1);
+	}
+
+	public int getValue(String s) {
+		String val = s.substring(1);
+
+		if (val.equals("A")) {
+
+		} else if (val.equals("K")) {
+
+		} else if (val.equals("Q")) {
+
+		} else if (val.equals("J")) {
+
+		} else {
+			return Integer.parseInt(val);
+		}
+		return -1;
+	}
+
 }
