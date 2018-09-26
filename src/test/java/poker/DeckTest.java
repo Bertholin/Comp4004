0b53b66b9a2ad66dfa1d6c3511f2f8e9b23a7330
Bert <bertholindestinvil@gmail.com>
2018-09-25 22:34:45 -0400

    refactoring deck test code

--- src/test/java/poker/DeckTest.java
+++ src/test/java/poker/DeckTest.java
@@ -1,25 +1,28 @@
 package poker;
 
-public class DeckTest {
+import junit.framework.TestCase;
+
+public class DeckTest extends TestCase{
 	public void testopenFile() {
-		Hand hand = new Hand();
-		assertEquals(true, hand.file.openFile("cards.txt"));
+		Deck deck = new Deck();
+		assertEquals(true, deck.openFile("cards.txt"));
 	}
 
 	public void testCloseFile() {
-		Hand hand = new Hand();
-		assertEquals(true, hand.file.closeFile("cards.txt"));
+		Deck deck = new Deck();
+		assertEquals(true, deck.closeFile());
 	}
 
 	public void testDraw() {
+		Deck deck = new Deck();
 		Hand hand = new Hand();
-		hand.file.openFile("cards.txt");
+		deck.openFile("cards.txt");
 
-		hand.add(hand.draw());
-		hand.add(hand.draw());
-		hand.add(hand.draw());
+		hand.add(deck.draw());
+		hand.add(deck.draw());
+		hand.add(deck.draw());
 
 		assertEquals(3, hand.size());
-		hand.file.close("cards.txt");
+		deck.closeFile();
 	}
 }
