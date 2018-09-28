33761f85ee2456998f4fd8bc0b0d0b49ecedcd7d
Bert <bertholindestinvil@gmail.com>
2018-09-27 21:30:04 -0400

    added test for flush and cards

--- src/test/java/poker/GameTest.java
+++ src/test/java/poker/GameTest.java
@@ -3,7 +3,45 @@ package poker;
 import junit.framework.TestCase;
 
 public class GameTest extends TestCase{
-	public void testFiveOfAKind() {
+	public void testFlush() {
+		Game game = new Game();
+		Deck d = new Deck();
+		d.openFile("flushtest.txt");
+		
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		assertEquals(false, game.isFlush(game.hand));
+		
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		assertEquals(false, game.isFlush(game.hand));
+		
+		game.hand.removeAll();
+		game.AIHand.removeAll();
+		
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		assertEquals(false, game.isFlush(game.hand));
+		
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		assertEquals(true, game.isFlush(game.hand));
+
+	}
+	
+	public void testHighCard() {
 		
 	}
 	
@@ -15,40 +53,7 @@ public class GameTest extends TestCase{
 		
 	}
 	
-	public void testFlush() {
-		Game game = new Game();
-		game.hand.add("CJ");
-		game.hand.add("D1");
-		game.hand.add("D9");
-		game.hand.add("D7");
-		game.hand.add("D3");
-		assertEquals(false, game.isFlush(game.hand));
-		
-		game.AIHand.add("SQ");
-		game.AIHand.add("S3");
-		game.AIHand.add("S4");
-		game.AIHand.add("S6");
-		game.AIHand.add("HA");
-		assertEquals(false, game.isFlush(game.hand));
-		
-		game.hand.removeAll();
-		game.AIHand.removeAll();
-		
-		game.hand.add("HK");
-		game.hand.add("H4");
-		game.hand.add("S10");
-		game.hand.add("H6");
-		game.hand.add("H7");
-		assertEquals(false, game.isFlush(game.hand));
-		
-		game.AIHand.add("D8");
-		game.AIHand.add("D5");
-		game.AIHand.add("D1");
-		game.AIHand.add("DK");
-		game.AIHand.add("DJ");
-		assertEquals(true, game.isFlush(game.hand));
-
-	}
+	
 	
 	public void testStraight() {
 		
