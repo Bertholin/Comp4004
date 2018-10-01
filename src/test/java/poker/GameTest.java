e21235297b658ba9656df13fe404136b94458ff5
Bert <bertholindestinvil@gmail.com>
2018-09-30 22:32:53 -0400

    created the test for highcard..
    -testing highcard
      -first
      -last
      -middle
      -duplicate high card

--- src/test/java/poker/GameTest.java
+++ src/test/java/poker/GameTest.java
@@ -48,8 +48,43 @@ public class GameTest extends TestCase{
 	public void testHighCard() {
 		Game game = new Game();
 		Deck d = new Deck();
-		
+		d.openFile("highCard.txt");
 		String highest = ""; 
+		
+		//middle
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		assertEquals("D9", game.highestCard(game.hand));
+		
+		//first
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		game.AIHand.add(d.draw());
+		assertEquals("SJ", game.highestCard(game.AIHand));
+		
+		game.hand.removeAll();
+		game.AIHand.removeAll();
+		
+		//highest card last
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		assertEquals("D10", game.highestCard(game.hand));
+		
+		//multiple high cards gets results based on suit
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		game.hand.add(d.draw());
+		assertEquals("SK", game.highestCard(game.AIHand));
 		
 	}
 	
