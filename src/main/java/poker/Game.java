50450e23072af17820a9929bf203c334437cd614
Bert <bertholindestinvil@gmail.com>
2018-10-07 21:30:37 -0400

    added code for two pair and a helper function to find the value of the first pair

--- src/main/java/poker/Game.java
+++ src/main/java/poker/Game.java
@@ -68,6 +68,25 @@ public class Game {
 		return false;
 	}
 
+	public int findValueOfAKind(Hand h, int amount) {
+		int counter = 0;
+		int val = 0;
+		for (int i = 0; i < 5; ++i) {
+			counter = 0;
+			for (int j = 0; j < h.size(); ++j) {
+				if (h.getValue(h.get(i)) == h.getValue(h.get(j))) {
+					counter++;
+					val = h.getValue(h.get(i));
+				}
+
+				if (counter == amount) {
+					return val;
+				}
+			}
+		}
+		return -1;
+	}
+
 	public boolean isStraight(Hand h) {
 		int min = h.getValue(h.get(0));
 		int counter = 0;
@@ -98,11 +117,11 @@ public class Game {
 
 		return false;
 	}
-	
+
 	public boolean isStraightFlush(Hand h) {
 		return (isStraight(h) && isFlush(h));
 	}
-	
+
 	public boolean isRoyalFlush(Hand h) {
 		int min = h.getValue(h.get(0));
 		for (int i = 1; i < h.size(); ++i)
@@ -113,7 +132,33 @@ public class Game {
 		if (min == 10) {
 			return (isStraight(h) && isFlush(h));
 		}
-		
+
 		return (false && isFlush(h));
+	}
+
+	public boolean isTwoPair(Hand h) {
+
+		if (ofAKind(h, 2)) {
+			int pair = findValueOfAKind(h, 2);
+			System.out.println("The value of the pair is " + pair);
+
+			for (int i = 0; i < h.size(); ++i)
+				System.out.println("The values of the cards are : " + h.get(i));
+
+			int counter = 0;
+			for (int i = 0; i < h.size(); ++i) {
+				counter = 0;
+				for (int j = 0; j < h.size(); ++j) {
+					if (pair != h.getValue(h.get(i)))
+						if (h.getValue(h.get(i)) == h.getValue(h.get(j)))
+							counter++;
+
+					if (counter == 2) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 }
